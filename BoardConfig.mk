82d34225a391f6abad5f5d701f442c33006eaaca
Jennifer Aaronsson <jenniferaaronsson@gmail.com>
2019-10-09 12:46:28 +0200

    Revert "Add workaround for slow flash"
    This reverts commit cb7e3f1cb4ac72ff08060b93d7ad7d3233564403.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -194,8 +194,4 @@ endif
 # Legacy blob support
 TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
     /vendor/bin/mm-qcamera-daemon=27
-
-# Enable workaround for slow rom flash
-BOARD_SUPPRESS_SECURE_ERASE := true
-
 TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT := true
